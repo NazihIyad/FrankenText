0cb3e7af12bf7433158f9157576a6cf05354dfc0
Your Name <you@example.com>
2025-11-19 17:19:24 +0100

    Changes

--- main.c
+++ main.c
@@ -1,82 +1,169 @@
+#include <ctype.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <time.h>
-#include <unistd.h>
 
-#define MAX_TOKENS 1000
+#define MAX_WORD_COUNT 15000
+#define MAX_SUCCESSOR_COUNT (MAX_WORD_COUNT / 2)
 
-char book[] =
-    "The monster screamed! The monster didn't run away. "
-    "The monster ate the food! The monster danced! "
-    "The monster cried! The monster laughed!";
+char* load_book(const char *filename) {
+    FILE *f = fopen(filename, "rb");
+    if (!f) {
+        printf("Could not open %s\n", filename);
+        exit(1);
+    }
 
-char *tokens[MAX_TOKENS];
-char *next_word[MAX_TOKENS];
-int token_count = 0;
+    fseek(f, 0, SEEK_END);
+    long size = ftell(f);
+    rewind(f);
+
+    char *buffer = malloc(size + 1);
+    if (!buffer) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+
+    fread(buffer, 1, size, f);
+    buffer[size] = '\0';
+
+    fclose(f);
+    return buffer;
+}
+
+char *book; 
+char *tokens[MAX_WORD_COUNT];
+size_t tokens_size = 0;
+
+char *succs[MAX_WORD_COUNT][MAX_SUCCESSOR_COUNT];
+size_t succs_sizes[MAX_WORD_COUNT];
 
 void replace_non_printable_chars_with_space() {
-    for (int i = 0; book[i] != '\0'; i++) {
+    for (size_t i = 0; book[i] != '\0'; i++) {
         if (!isprint((unsigned char)book[i])) {
             book[i] = ' ';
         }
     }
 }
 
-int find_token_id(char *word) {
-    for (int i = 0; i < token_count; i++) {
-        if (strcmp(tokens[i], word) == 0)
-            return i;
-    }
-    tokens[token_count] = word;
-    token_count++;
-    return token_count - 1;
-}
-
-void tokenize_and_fill() {
-    char *delim = " \n\r";
-    char *prev = NULL;
-    char *word = strtok(book, delim);
-
-    while (word != NULL) {
-        int id = find_token_id(word);
-        if (prev != NULL) {
-            next_word[find_token_id(prev)] = word;
+size_t token_id(char *token) {
+    for (size_t id = 0; id < tokens_size; ++id) {
+        if (strcmp(tokens[id], token) == 0) {
+            return id;
         }
-        prev = word;
-        word = strtok(NULL, delim);
+    }
+    tokens[tokens_size] = token;
+    return tokens_size++;
+}
+
+void append_to_succs(char *token, char *succ) {
+    size_t id = token_id(token);
+    size_t *next_empty_index_ptr = &succs_sizes[id];
+
+    if (*next_empty_index_ptr >= MAX_SUCCESSOR_COUNT) {
+        printf("Successor array full.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    succs[id][(*next_empty_index_ptr)++] = succ;
+}
+
+void tokenize_and_fill_succs(char *delimiters, char *str) {
+    char *prev = NULL;
+    char *tok = strtok(str, delimiters);
+
+    while (tok != NULL) {
+        token_id(tok);
+        if (prev != NULL) {
+            append_to_succs(prev, tok);
+        }
+        prev = tok;
+        tok = strtok(NULL, delimiters);
     }
 }
 
-int token_ends_sentence(char *word) {
-    char c = word[strlen(word) - 1];
-    return (c == '.' || c == '?' || c == '!');
+char last_char(char *str) {
+    size_t len = strlen(str);
+    if (len == 0) return '\0';
+    return str[len - 1];
+}
+
+bool token_ends_a_sentence(char *token) {
+    char c = last_char(token);
+    return (c == '.' || c == '!' || c == '?');
+}
+
+size_t random_token_id_that_starts_a_sentence() {
+    size_t candidates[MAX_WORD_COUNT];
+    size_t count = 0;
+
+    for (size_t i = 0; i < tokens_size; i++) {
+        if (isupper((unsigned char)tokens[i][0])) {
+            candidates[count++] = i;
+        }
+    }
+
+    if (count == 0) return 0;
+    return candidates[rand() % count];
+}
+
+char *generate_sentence(char *sentence, size_t sentence_size) {
+    size_t current_token_id = random_token_id_that_starts_a_sentence();
+    char *token = tokens[current_token_id];
+
+    sentence[0] = '\0';
+    strcat(sentence, token);
+    if (token_ends_a_sentence(token)) return sentence;
+
+    size_t sentence_len_next;
+
+    do {
+        size_t succ_count = succs_sizes[current_token_id];
+        if (succ_count == 0) break;
+
+        size_t next = rand() % succ_count;
+        char *next_tok = succs[current_token_id][next];
+
+        sentence_len_next = strlen(sentence) + 1 + strlen(next_tok);
+        if (sentence_len_next >= sentence_size - 1) break;
+
+        strcat(sentence, " ");
+        strcat(sentence, next_tok);
+
+        if (token_ends_a_sentence(next_tok)) break;
+
+        current_token_id = token_id(next_tok);
+    } while (1);
+
+    return sentence;
 }
 
 int main() {
-    srand(time(NULL));
+    printf("Program started.\n");
+
+    book = load_book("../pg84.txt"); 
+    printf("Loaded %zu characters.\n", strlen(book));
 
     replace_non_printable_chars_with_space();
-    tokenize_and_fill();
 
-    sleep(1); 
+    char *delimiters = " \n\r";
+    tokenize_and_fill_succs(delimiters, book);
 
-    int start;
+    char sentence[1000];
+    srand((unsigned int)time(NULL));
+
     do {
-        start = rand() % token_count;
-    } while (!isupper(tokens[start][0]));
+        generate_sentence(sentence, sizeof(sentence));
+    } while (last_char(sentence) != '?');
+    puts(sentence);
+    puts("");
 
-    printf("%s", tokens[start]);
+    do {
+        generate_sentence(sentence, sizeof(sentence));
+    } while (last_char(sentence) != '!');
+    puts(sentence);
 
-    char *word = next_word[start];
-    while (word != NULL) {
-        printf(" %s", word);
-        if (token_ends_sentence(word))
-            break;
-        word = next_word[find_token_id(word)];
-    }
-
-    printf("\n");
-    return 0;
+    free(book); 
 }
